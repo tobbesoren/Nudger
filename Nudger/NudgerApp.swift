865bc01a646a22713b45daa57e67f03e59319d1e
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-04-23 10:56:57 +0200

    Initial Commit

---
+++ Nudger/NudgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NudgerApp.swift
+//  Nudger
+//
+//  Created by Tobias Sörensson on 2023-04-23.
+//
+
+import SwiftUI
+
+@main
+struct NudgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
