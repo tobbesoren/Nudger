a29c15e5c3f839bb3a8fc2798c0aecb13ca28107
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-04-27 12:58:50 +0200

    startingOut

--- Nudger/ContentView.swift
+++ Nudger/ContentView.swift
@@ -6,21 +6,110 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
+    @State var signedIn = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if !signedIn {
+            SignInView(signedIn: $signedIn)
+        } else {
+            NudgesView()
         }
-        .padding()
     }
 }
+
+
+struct SignInView: View {
+    @Binding var signedIn: Bool
+    var auth = Auth.auth()
+    
+    var body: some View {
+        Button(action: {
+            auth.signInAnonymously { result, error in
+                if let error {
+                    print(error)
+                } else {
+                    signedIn = true
+                }
+            }
+        }) {
+            Text("Sign in")
+        }
+    }
+}
+
+
+struct NudgesView: View {
+    
+    @StateObject var nudgesVM = NudgesVM()
+    @State var showingAddAlert = false
+    @State var newNudgeName = ""
+    @State var date = Date()
+    @State var todaysNudges: [Nudge]?
+    
+    var body: some View {
+        VStack {
+            DatePicker("Hmm", selection: $date)
+            List {
+                ForEach(nudgesVM.nudges) { nudge in
+                    RowView(nudge: nudge, vm: nudgesVM)
+                }
+                .onDelete() { indexSet in
+                    for index in indexSet {
+                        nudgesVM.deleteFromFirestore(index: index)
+                    }
+                }
+            }
+            Button(action: {
+                showingAddAlert = true
+            }) {
+                Text("Add")
+            }
+            .alert("Add", isPresented: $showingAddAlert) {
+                TextField("Add", text: $newNudgeName)
+                Button("Add", action: { nudgesVM.saveToFirestore(nudgeName: newNudgeName, description: "---", dateCreated: date)
+                    newNudgeName = ""
+                })
+            }
+        }.onAppear() {
+          nudgesVM.listenToFirestore()
+        }
+    }
+    
+    
+
+    
+    
+    private struct RowView: View {
+        let nudge: Nudge
+        let vm: NudgesVM
+        
+        var body: some View {
+            HStack {
+                Text(nudge.name)
+                Spacer()
+                Button(action: {
+                    vm.setDone(nudge: nudge)
+                }) {
+                    if let latestDone = nudge.doneDates.last {
+                        Image(systemName: Calendar.current.isDate(latestDone, equalTo: Date(), toGranularity: .day)  ? "checkmark.square" : "square")
+                    } else {
+                        Image(systemName: "square")
+                    }
+                }.buttonStyle(.borderless)
+            }
+        }
+    }
+}
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NudgesView()
     }
 }
+
---
+++ Nudger/ViewModel/NudgesVM.swift
@@ -0,0 +1,105 @@
+//
+//  NudgesVM.swift
+//  Nudger
+//
+//  Created by Tobias Sörensson on 2023-04-23.
+//
+
+import Foundation
+import Firebase
+
+class NudgesVM: ObservableObject {
+    @Published var nudges = [Nudge]()
+    let db = Firestore.firestore()
+    let auth = Auth.auth()
+    var currentNudges = [Nudge]()
+    
+    
+    func setDone(nudge: Nudge) {
+        guard let user = auth.currentUser else {return}
+        let nudgeRef = db.collection("users").document(user.uid).collection("nudges")
+
+        if let id = nudge.id {
+            if let latestDone = nudge.doneDates.last {
+                if !Calendar.current.isDate(latestDone, equalTo: Date(), toGranularity: .day) {
+                    nudgeRef.document(id).updateData(["doneDates" : FieldValue.arrayUnion([Date()])])
+                    return // Added missing return statement
+                } else {
+                    // Here i should add code for removing the last Date object for the list. Tricky, since Firebase doesn't save
+                    // them in the same format as Swift uses. If I get this to work, change func name to 'toggleDone'.
+                    return
+                }
+            }
+            nudgeRef.document(id).updateData(["doneDates" : FieldValue.arrayUnion([Date()])])
+        }
+    }
+    
+    
+    func saveToFirestore(nudgeName: String, description: String, dateCreated: Date) {
+        guard let user = auth.currentUser else {return}
+        let nudgeRef = db.collection("users").document(user.uid).collection("nudges")
+        
+        let nudge = Nudge(name: nudgeName, description: description, dateCreated: dateCreated)
+        do {
+            let _ = try nudgeRef.addDocument(from: nudge)
+        } catch {
+            print("Error saving to db.\(error)")
+        }
+    }
+    
+    
+    func checkStreak() {
+        
+    }
+    
+    func deleteDate(date: Date, nudgeRef: CollectionReference) {
+        // Easier said than done!
+    }
+    
+    
+    func deleteFromFirestore(index: Int) {
+        
+        guard let user = auth.currentUser else {return}
+        let nudgeRef = db.collection("users").document(user.uid).collection("nudges")
+        let nudge = nudges[index]
+        
+        if let id = nudge.id {
+            nudgeRef.document(id).delete()
+        }
+    }
+    
+    func setCurrentNudges() {
+        for nudge in nudges {
+            // If the nudge should be triggered today, add it to currentNudges
+        }
+        
+    }
+    
+    
+    func listenToFirestore() {
+        guard let user = auth.currentUser else {return}
+        let nudgeRef = db.collection("users").document(user.uid).collection("nudges")
+        
+        nudgeRef.addSnapshotListener() {
+            snapshot, err in
+            
+            guard let snapshot = snapshot else {return}
+            
+            if let err = err {
+                print("Error getting document \(err)")
+            } else {
+                self.nudges.removeAll()
+                for document in snapshot.documents {
+                    do {
+                        let nudge = try document.data(as: Nudge.self)
+                        self.nudges.append(nudge)
+                    } catch {
+                        print("Error generating list \(error)")
+                    }
+                }
+                
+            }
+        }
+    }
+}
+
